4db1bdc0c6352b8ae3047dd4056dec292ecb352f
Halil Uysal <uysal.halil@outlook.com>
2024-08-04 15:07:05 -0400

    removed the description of treenode from the file

--- dsa-interview-crash-course/0111_minimum_depth_of_binary_tree.cpp
+++ dsa-interview-crash-course/0111_minimum_depth_of_binary_tree.cpp
@@ -1,14 +1,6 @@
 #include <cmath>
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right)
-        : val(x), left(left), right(right) {}
-};
+#include "TreeNode.h"
 
 class Solution {
    public:
