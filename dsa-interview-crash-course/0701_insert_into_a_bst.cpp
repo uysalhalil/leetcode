e200557ecf984ad7a777021cfedc3cd6f4eaa17b
Halil Uysal <uysal.halil@outlook.com>
2024-08-11 15:32:55 -0400

    insertion is the simmplest way possible, it is not utilizing balancing of the tree.

---
+++ dsa-interview-crash-course/0701_insert_into_a_bst.cpp
@@ -0,0 +1,19 @@
+#include "TreeNode.h"
+
+class Solution {
+   public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if (!root) {
+            TreeNode* node = new TreeNode(val);
+            return node;
+        }
+
+        if (val < root->val) {
+            root->left = insertIntoBST(root->left, val);
+        } else {
+            root->right = insertIntoBST(root->right, val);
+        }
+
+        return root;
+    }
+};
