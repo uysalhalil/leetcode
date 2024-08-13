7ac48e0382a698a1a97884b360784270a1c0385e
Halil Uysal <uysal.halil@outlook.com>
2024-08-12 22:32:59 -0400

    dfs version

--- dsa-interview-crash-course/0841_keys_and_rooms.cpp
+++ dsa-interview-crash-course/0841_keys_and_rooms.cpp
@@ -4,25 +4,48 @@
 class Solution {
    private:
     std::vector<bool> visited;  // visited rooms
-   public:
-    bool canVisitAllRooms(std::vector<std::vector<int>> &rooms) {
-        visited.resize(rooms.size(), false);
-        visited[0] = true;
-        std::stack<std::vector<int>> nextRooms;
-        nextRooms.push(rooms[0]);
-        while (!nextRooms.empty()) {
-            auto neighbors = nextRooms.top();
-            nextRooms.pop();
-            for (auto &neighbor : neighbors) {
-                if (!visited[neighbor]) {
-                    visited[neighbor] = true;
-                    nextRooms.push(rooms[neighbor]);
-                }
-            }
+
+    void dfs(const std::vector<std::vector<int>>& rooms, int currentRoom) {
+        if (visited[currentRoom]) return;
+        visited[currentRoom] = true;
+        for (auto neighbor : rooms[currentRoom]) {
+            dfs(rooms, neighbor);
         }
-        for (auto &&v : visited) {
+    }
+
+   public:
+    bool canVisitAllRooms(std::vector<std::vector<int>>& rooms) {
+        visited.resize(rooms.size(), false);
+        dfs(rooms, 0);
+        for (auto v : visited) {
             if (!v) return false;
         }
         return true;
     }
 };
+
+// class Solution {
+//    private:
+//     std::vector<bool> visited;  // visited rooms
+//    public:
+//     bool canVisitAllRooms(std::vector<std::vector<int>> &rooms) {
+//         visited.resize(rooms.size(), false);
+//         visited[0] = true;
+//         std::stack<std::vector<int>> nextRooms;
+//         nextRooms.push(rooms[0]);
+//         while (!nextRooms.empty()) {
+//             auto neighbors = nextRooms.top();
+//             nextRooms.pop();
+//             for (auto &neighbor : neighbors) {
+//                 if (!visited[neighbor]) {
+//                     visited[neighbor] = true;
+//                     nextRooms.push(rooms[neighbor]);
+//                 }
+//             }
+//         }
+//         for (auto &&v : visited) {
+//             if (!v) return false;
+//         }
+//         return true;
+//     }
+// };
