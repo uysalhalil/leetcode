3e7959d8a932c8926c625a82df07a137dcdcf36e
Halil Uysal <uysal.halil@outlook.com>
2024-08-12 22:23:58 -0400

    simple iterataive traversal. I am not sure if this is counted as graph.

---
+++ dsa-interview-crash-course/0841_keys_and_rooms.cpp
@@ -0,0 +1,29 @@
+#include <stack>
+#include <vector>
+
+class Solution {
+   private:
+    std::vector<bool> visited;  // visited rooms
+   public:
+    bool canVisitAllRooms(std::vector<std::vector<int>> &rooms) {
+        visited.resize(rooms.size(), false);
+        visited[0] = true;
+    std:
+        std::stack<std::vector<int>> nextRooms;
+        nextRooms.push(rooms[0]);
+        while (!nextRooms.empty()) {
+            auto neighbors = nextRooms.top();
+            nextRooms.pop();
+            for (auto &neighbor : neighbors) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    nextRooms.push(rooms[neighbor]);
+                }
+            }
+        }
+        for (auto &&v : visited) {
+            if (!v) return false;
+        }
+        return true;
+    }
+};
