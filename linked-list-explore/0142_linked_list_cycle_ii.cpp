453650ad56e06bb97a135b91ec0b891d9e21762e
Halil Uysal <uysal.halil@outlook.com>
2024-08-22 21:03:53 -0400

    I need to be able to prove this.

---
+++ linked-list-explore/0142_linked_list_cycle_ii.cpp
@@ -0,0 +1,24 @@
+#include "ListNode.h"
+
+class Solution {
+   public:
+    ListNode* detectCycle(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (fast == slow) {
+                ListNode* slow2 = head;
+                while (slow != slow2) {
+                    slow = slow->next;
+                    slow2 = slow2->next;
+                }
+                return slow;
+            }
+        }
+        return nullptr;
+    }
+};
