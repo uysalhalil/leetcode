cbaf62f515a25e74e3b0d23ebbd075d906ce97e7
Halil Uysal <uysal.halil@outlook.com>
2024-08-23 21:54:27 -0400

    asd

---
+++ linked-list-explore/0328_odd_even_linked_list.cpp
@@ -0,0 +1,35 @@
+#include "ListNode.h"
+
+class Solution {
+   private:
+    int _size{0};
+    ListNode* getTail(ListNode* head) {
+        ListNode* curr{head};
+        while (curr->next) {
+            curr = curr->next;
+            ++_size;
+        }
+        return curr;
+    }
+
+   public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (!head || !head->next || !head->next->next) return head;
+
+        ListNode* tail = getTail(head);
+        ListNode* movingTail{tail};
+        ListNode* curr = head;
+        int i = 0;
+        while (i < _size) {
+            ListNode* moveToEnd = curr->next;
+            curr->next = curr->next->next;
+            moveToEnd->next = nullptr;
+            movingTail->next = moveToEnd;
+            movingTail = moveToEnd;
+            curr = curr->next;
+            i += 2;
+        }
+
+        return head;
+    }
+};
