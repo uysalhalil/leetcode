ce6249a77424ec3110bc732712b0e92cdaa37355
Halil Uysal <uysal.halil@outlook.com>
2024-08-26 21:41:18 -0400

    default question for heap

---
+++ explores/heap/0215_kth_largeest_element_in_an_array.cpp
@@ -0,0 +1,11 @@
+#include <queue>
+#include <vector>
+
+class Solution {
+   public:
+    int findKthLargest(std::vector<int>& nums, int k) {
+        std::priority_queue<int> maxHeap(nums.begin(), nums.end());
+        for (int i = 0; i < k - 1; ++i) maxHeap.pop();
+        return maxHeap.top();
+    }
+};
