3a1b69270463f6fcb3f88d99d5854d6c9c3beee5
Halil Uysal <uysal.halil@outlook.com>
2024-09-12 21:32:51 -0400

    like binary search doubling down

---
+++ explores/recursion-i/0050_pow.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+
+class Solution {
+   public:
+    double myPow(double x, int n) {
+        if (n == 0) return 1;
+        if (n < 0) {
+            n = std::abs(n);
+            x = 1 / x;
+        }
+        if (n % 2 == 0)
+            return myPow(x * x, n / 2);
+        else
+            return x * myPow(x, n - 1);
+    }
+};
