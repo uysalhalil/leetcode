8482af7333508d835305cc396c507c51e52bc367
Halil Uysal <uysal.halil@outlook.com>
2025-01-30 21:10:33 -0500

    generic n-ary node

---
+++ explores/n-ary-tree/Node.py
@@ -0,0 +1,7 @@
+from typing import List, Optional
+
+
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
