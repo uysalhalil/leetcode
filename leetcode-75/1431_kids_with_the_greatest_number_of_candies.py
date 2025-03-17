14c2a1a1b86c4c8e6cd7c83f38b530bd3ff2f74a
Halil Uysal <uysal.halil@outlook.com>
2025-03-16 21:06:37 -0400

    simple one pointer

---
+++ leetcode-75/1431_kids_with_the_greatest_number_of_candies.py
@@ -0,0 +1,9 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        result = [False] * len(candies)
+        maxValue = max(candies)
+        for i in range(len(candies)):
+            if candies[i] + extraCandies >= maxValue:
+                result[i] = True
+
+        return result
